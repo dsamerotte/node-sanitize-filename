a777d50b80330b3f7a6c6a08f756d867694ebfbb
Darius Samerotte <darius@NeXt.local>
2018-05-06 17:48:49 -0700

    * Add eslint airbnb and update code for the same; * Remove unnecessary regex espaces.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/*jshint node:true*/
-'use strict';
+/* jshint node:true */
 
 /**
  * Replaces characters in strings that are illegal/unsafe for filenames.
@@ -28,16 +27,16 @@
  * @return {String}         Sanitized filename
  */
 
-var truncate = require("truncate-utf8-bytes");
+const truncate = require('truncate-utf8-bytes');
 
-var illegalRe = /[\/\?<>\\:\*\|":]/g;
-var controlRe = /[\x00-\x1f\x80-\x9f]/g;
-var reservedRe = /^\.+$/;
-var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
-var windowsTrailingRe = /[\. ]+$/;
+const illegalRe = /[/?<>\\:*|":]/g;
+const controlRe = /[\x00-\x1f\x80-\x9f]/g; // eslint-disable-line no-control-regex
+const reservedRe = /^\.+$/;
+const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
+const windowsTrailingRe = /[. ]+$/;
 
 function sanitize(input, replacement) {
-  var sanitized = input
+  const sanitized = input
     .replace(illegalRe, replacement)
     .replace(controlRe, replacement)
     .replace(reservedRe, replacement)
@@ -46,9 +45,9 @@ function sanitize(input, replacement) {
   return truncate(sanitized, 255);
 }
 
-module.exports = function (input, options) {
-  var replacement = (options && options.replacement) || '';
-  var output = sanitize(input, replacement);
+module.exports = (input, options) => {
+  const replacement = (options && options.replacement) || '';
+  const output = sanitize(input, replacement);
   if (replacement === '') {
     return output;
   }
